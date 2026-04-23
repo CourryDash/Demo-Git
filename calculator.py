4c2a29e4bb6967a9c2a40f8fd0ac3fb147c8fe99
joseph06-code <josephbillytandy06@gmail.com>
2026-04-23 20:56:28 +0700

    Add divide feature

--- calculator.py
+++ calculator.py
@@ -7,3 +7,10 @@ def subtraction(a, b):
 
 print(add(10,5))
 print(subtraction(10, 5))
+
+def divide(num1, num2):
+    if num2 != 0:
+        pembagian = num1 / num2
+        return pembagian
+    else:
+        return "Error: Division by zero is not allowed."
